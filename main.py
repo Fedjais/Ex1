a1b6b930ec8bbccc4c52bfe9ec83801ede9df924
Fedjais <fedjawrites@gmail.com>
2024-05-14 21:48:29 +0300

    Patch #3

--- main.py
+++ main.py
@@ -1,53 +1,53 @@
 import random
 
-start = input('Ви запустили гру "Камінь, ножиці, папір". Для початку натисніть "+" або "-" для виходу: ')
+start = input('You have started a game of Rock, Paper, Scissors. To start, press "+" or "-" to exit: ')
 
 if start == '+':
-    print('Завантаження...')
-    print("Завантаження завершено! Починаємо гру!")
+    print('Download...')
+    print("Download complete! Let's start the game!")
     print("3...2...1...")
-    print("Якщо хочете вийти натисніть '-' .")
-    print("Хочете дізнатися рахунок натисніть '?'")
+    print("If you want to exit, press '-'.")
+    print("Want to know the score, press '?'")
     user_ball = 0
     rand_ball = 0
     while True:
-        user = input('Камінь-"к", Ножиці-"н" чи Папір-"п"?\n')
-        list_play = ['к','н','п']
+        user = input('Rock-"r", Scissors-"s" or Paper-"p"?\n')
+        list_play = ['r','s','p']
         if user in list_play:
             rand = random.choice(list_play)
             print(rand)
 
-            if rand == 'к' and user == "н":
+            if rand == 'r' and user == "s":
                 rand_ball += 1
-                print("Ви програли :(")
-            if rand == 'н' and user == "п":
+                print("U luze :(")
+            if rand == 's' and user == "p":
                 rand_ball += 1
-                print("Ви програли :(")
-            if rand == 'п' and user == "к":
+                print("U luze :(")
+            if rand == 'p' and user == "r":
                 rand_ball += 1
-                print("Ви програли :(")
-            if rand == 'к' and user == "п":
+                print("U luze :(")
+            if rand == 'r' and user == "p":
                 user_ball += 1
-                print("Ви перемогли :)")
-            if rand == 'н' and user == "к":
+                print("U win :)")
+            if rand == 's' and user == "r":
                 user_ball += 1
-                print("Ви перемогли :)")
-            if rand == 'п' and user == "н":
+                print("U win :)")
+            if rand == 'p' and user == "s":
                 user_ball += 1
-                print("Ви перемогли :)")
+                print("U win :)")
             if rand == user:
-                print("Нічия")
+                print("Tie :/")
         elif user == "?":
-            print(f'Ваші бали - {user_ball}, Бали вашого суперника - {rand_ball}.')
+            print(f'Ur Score - {user_ball}, Enemmy Score - {rand_ball}.')
         elif user == "-":
-            print(f'Ваші бали - {user_ball}, Бали вашого суперника - {rand_ball}.')
-            print('Кінець гри, Допобачення!')
+            print(f'Ur Score - {user_ball}, Enemmy Score - {rand_ball}.')
+            print('End game, Bye!')
             break
         else:
-            print("Ведіть ",'к','н','п')
+            print("Input ",'r','s','p')
 
 if start == "-":
-    print("Дуже сумно, допобачення")
+    print("Sad, Bye")
 else:
-    print("Я вас не зрозумів, перезапустіть программу.")
+    print("I don`t undershtand.")
 
